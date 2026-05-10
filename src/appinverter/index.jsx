dde86fc89b98fedd8114f3c3e878e8808d9241f7
chrisjaron <chrisjaron@karunya.edu.in>
2026-05-10 09:52:05 +0530

    deploy

--- src/appinverter/index.jsx
+++ src/appinverter/index.jsx
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2026 Creoleap Technologies Pvt. Ltd.
  * All rights reserved. Proprietary and confidential.
- * Unauthorized copying, distribution, or modification is strictly prohibited. deploy
+ * Unauthorized copying, distribution, or modification is strictly prohibited.
  */
 import React, { useState, useEffect } from 'react';
 import { useAppState } from './hooks/useAppState';
